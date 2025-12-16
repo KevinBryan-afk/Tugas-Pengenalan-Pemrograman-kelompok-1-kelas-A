92340dd453706954b40df03a62c6b8ade47dca6b
KevinBryan-afk <kevinbryanvanas@gmail.com>
2025-12-16 11:00:15 +0800

    Tambahkan Operasi dalam kalkulator
    Added operation selection and input for two numbers.

--- Kalkulator_Sederhana.py
+++ Kalkulator_Sederhana.py
@@ -1,4 +1,14 @@
 def main():
   print("Kalkulator Sederhana")
+  print("1. Penjumlahan")
+  print("2. Pengurangan")
+  print("3. Perkalian")
+  print("4. Pembagian")
 
-main()
+pilihan = input("Pilih Operasi yang ingin digunakan. (1/2/3/4) ")
+
+a = float(input("angka pertama: "))
+b =  float(input("angka kedua: "))
+
+if __name__ == "__main__":
+  main()
