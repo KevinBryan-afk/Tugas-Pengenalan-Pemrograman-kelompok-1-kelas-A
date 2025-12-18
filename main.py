d3f73bfe022465edc99480843ad9883a975b6438
KevinBryan-afk <kevinbryanvanas@gmail.com>
2025-12-18 10:30:24 +0800

    Refactor main function for better flow and clarity

--- main.py
+++ main.py
@@ -9,28 +9,37 @@ def tampilkan_menu():
 
 def input_angka():
   a = float(input("angka pertama: "))
-  b =  float(input("angka kedua: "))
+  b = float(input("angka kedua: "))
   return a, b
 
 def main():
-  tampilkan_menu()
+  while True:
+    tampilkan_menu() 
+    pilihan = input("Pilih operasi (0/1/2/3/4): ")
+    if pilihan == "0":
+     print("Program dihentikan.")
+     break
+     
+    if pilihan not in ("1", "2", "3", "4"):
+       print("Pilihan tidak valid.")
+       continue
+
+
+    a, b = input_angka()
   
-  pilihan = input("Pilih Operasi yang ingin digunakan. (1/2/3/4) ")
-  a, b = input_angka()
+    if pilihan == 1:
+      hasil = tambah(a,b)
+    elif pilihan == 2:
+      hasil = kurang(a,b)
+    elif pilihan == 3:
+      hasil = kali(a,b)
+    elif pilihan == 4:
+      hasil = bagi(a,b)
+    else:
+      print("Pilihan tidak valid!")
+      return
   
-  if pilihan == 1:
-    hasil = tambah(a,b)
-  elif pilihan == 2:
-    hasil = kurang(a,b)
-  elif pilihan == 3:
-    hasil = kali(a,b)
-  elif pilihan == 4:
-    hasil = bagi(a,b)
-  else:
-    print("Pilihan tidak valid!")
-    return
-  
-  print("Hasil:", hasil)
+    print("Hasil:", hasil)
 
 if __name__ == "__main__":
   main()
