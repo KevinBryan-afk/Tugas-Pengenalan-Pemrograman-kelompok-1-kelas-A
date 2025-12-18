25ebe145ccd6d9108993f6663bbce46dddeaa6e8
KevinBryan-afk <kevinbryanvanas@gmail.com>
2025-12-18 09:36:30 +0800

    Add addition and subtraction to calculator
    Refactor calculator to include addition and subtraction features, and fix function definitions.

--- Kalkulator_Sederhana.py
+++ Kalkulator_Sederhana.py
@@ -1,15 +1,36 @@
 from fitur_perkalian_dan_pembagian.py import kali,bagi
-def main():
+from fitur_tambah_kurang.py import tambah,kurang
+def tampilkan_menu():
   print("Kalkulator Sederhana")
   print("1. Penjumlahan")
   print("2. Pengurangan")
   print("3. Perkalian")
   print("4. Pembagian")
 
-  pilihan = input("Pilih Operasi yang ingin digunakan. (1/2/3/4) ")
-
+def input_angka()
   a = float(input("angka pertama: "))
   b =  float(input("angka kedua: "))
+  return a, b
+
+def main()
+  tampilkan_menu()
+  
+  pilihan = input("Pilih Operasi yang ingin digunakan. (1/2/3/4) ")
+  a, b = input_angka()
+  
+  if pilihan == 1:
+    hasil = tambah(a,b)
+  elif pilihan == 2:
+    hasil = kurang(a,b)
+  elif pilihan == 3:
+    hasil = kali(a,b)
+  elif pilihan == 4:
+    hasil = bagi(a,b)
+  else:
+    print("Pilihan tidak valid!")
+    return
+  
+  print("Hasil:", hasil)
 
 if __name__ == "__main__":
   main()
